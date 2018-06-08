26590c78bf6f47f07b3ad06d978320465fc1c3b8
Jaime Radulovich <jradlov@gmail.com>
2018-06-08 09:41:58 -0700

    Add project files.

---
+++ TableView_0/TableView_0/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TableView_0
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void EntryCell_Completed(object sender, EventArgs e)
+        {
+            string str = entry1.Text;
+            DisplayAlert("Entry Cell", str, "Ok");
+        }
+
+        private void SwitchCell_OnChanged(object sender, ToggledEventArgs e)
+        {
+            DisplayAlert("Switch Cell", e.Value.ToString(), "Ok");
+        }
+
+        private void DatePicker_DateSelected(object sender, DateChangedEventArgs e)
+        {
+            DisplayAlert("Date Selected", e.NewDate.ToLongDateString(), "Ok");
+        }
+    }
+}
